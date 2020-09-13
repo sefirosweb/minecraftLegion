76b845bf16e43cda43d07a01a75d9834e6ee6b0f
Sefirosweb <sefirosweb@gmail.com>
2020-09-13 19:31:18 +0200

    Added auto sleep near bed

--- web.js
+++ web.js
@@ -19,8 +19,7 @@ bot.on('kicked', (reason, loggedIn) => {
 
 bot.on('error', err => console.log(err))
 
-bot.once("spawn", () =>
-{
+bot.once("spawn", () => {
     bot.chat('/login ' + AUTOLOGIN)
     bot.chat('Im in!')
 })
@@ -43,51 +42,32 @@ const {
     NestedStateMachine,
     BehaviorMoveTo
 } = require("mineflayer-statemachine");
-    /*
-function goBase()
-{
-    console.log("GO")
-    
-    const mcData = require('minecraft-data')(bot.version)
-    const defaultMove = new Movements(bot, mcData)
 
-    const p = {};
-    p.x = -10;
-    p.y = 93;
-    p.z = -3;
-
-    bot.pathfinder.setMovements(defaultMove)
-    bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
-}
-*/
-bot.once("spawn", () =>
-{
-    // This targets object is used to pass data between different states. It can be left empty.
+bot.once("spawn", () => {
     const targets = {};
 
-    console.log("Current position: ")
-    console.log(bot.player.entity.position)
-    
-    // Create our states
-    /*
-    const mcData = require('minecraft-data')(bot.version)
-    const defaultMove = new Movements(bot, mcData)
+    const base = {};
+    base.position = {}
+    base.position.x = -3;
+    base.position.y = 80;
+    base.position.z = 0;
+    const goBase = new BehaviorMoveTo(bot, base);
 
-    const p = {};
-    p.x = -20;
-    p.y = 93;
-    p.z = -13;
+    // Movet o night timer
+    const bed = bot.findBlock({
+        matching: block => bot.isABed(block)
+    })
 
-    const goal = new GoalNear(p.x, p.y, p.z, 1)
-    const goBase = new BehaviorMoveTo(bot, targets);
-    */
-    const p = {};
-    p.position = {}
-    p.position.x = -20;
-    p.position.y = 93;
-    p.position.z = -13;
+    const bedPosition = {}
+    bedPosition.position = {}
+    bedPosition.position.x = bed.position.x;
+    bedPosition.position.y = bed.position.y;
+    bedPosition.position.z = bed.position.z;
 
-    const goBase = new BehaviorMoveTo(bot, p);
+    const checkIsNight = new checkNight(bot);
+    const goToBed = new BehaviorMoveTo(bot, bed);
+    const goSleep = new goBedAction(bot, bed);
+
     const printServerStates = new BehaviorPrintServerStats(bot);
     const idleState = new BehaviorIdle();
     const followPlayer = new BehaviorFollowEntity(bot, targets);
@@ -96,112 +76,143 @@ bot.once("spawn", () =>
     const lookAtPlayersState = new BehaviorLookAtEntity(bot, targets);
 
 
+
     // Create our transitions
     const transitions = [
 
         new StateTransition({ // 0
             parent: printServerStates,
             child: goBase,
+            name: '0 Starts',
             shouldTransition: () => true
         }),
 
         new StateTransition({ // 1
             parent: idleState,
             child: getClosestPlayer,
-            name: 'player says "hi"',
+            name: '1 player says "hi"',
             onTransition: () => bot.chat("Hi master!")
         }),
 
         new StateTransition({ // 2
             parent: getClosestPlayer,
             child: lookAtPlayersState,
+            name: '2 get close entity',
             shouldTransition: () => true,
         }),
 
         new StateTransition({ // 3
             parent: lookAtPlayersState,
             child: idleState,
-            name: 'player says "bye"',
+            name: '3 player says "bye"',
             onTransition: () => bot.chat("Good bye master!")
         }),
 
         new StateTransition({ // 4
             parent: lookAtPlayersState,
             child: followPlayer,
-            name: 'player says "come"',
+            name: '5 player says "come"',
             onTransition: () => bot.chat("Yes sr!")
         }),
 
         new StateTransition({ // 5
             parent: followPlayer,
             child: lookAtPlayersState,
-            name: 'player says "stay"',
+            name: '5 player says "stay"',
             onTransition: () => bot.chat("Yes sr!!")
         }),
 
         new StateTransition({ //  6
             parent: followPlayer,
             child: idleState,
-            name: 'player says "bye"',
+            name: '6 player says "bye"',
             onTransition: () => bot.chat("GB sr!")
         }),
 
         new StateTransition({ // 7
             parent: followPlayer,
             child: lookAtFollowTarget,
-            name: 'closeToTarget',
+            name: '7 closeToTarget',
             shouldTransition: () => followPlayer.distanceToTarget() < 2,
         }),
 
         new StateTransition({ // 8
             parent: lookAtFollowTarget,
             child: followPlayer,
-            name: 'farFromTarget',
+            name: '8 farFromTarget',
             shouldTransition: () => lookAtFollowTarget.distanceToTarget() >= 2,
         }),
 
         new StateTransition({ // 9
             parent: lookAtFollowTarget,
             child: idleState,
-            name: 'player says "bye"',
+            name: '9 player says "bye"',
             onTransition: () => bot.chat("goodbye")
         }),
 
         new StateTransition({ // 10
             parent: lookAtFollowTarget,
             child: lookAtPlayersState,
-            name: 'player says "stay"',
-        }),    
+            name: '10 player says "stay"',
+        }),
 
         new StateTransition({ // 11
             parent: goBase,
             child: idleState,
-            name: 'Near base',
+            name: '11 Near base',
             shouldTransition: () => goBase.distanceToTarget() < 2,
             onTransition: () => {
-                bot.chat("I go basee!")
+                bot.chat("Im are on base!")
             },
         }),
 
         new StateTransition({ // 12
-            parent: idleState,
-            child: goBase,
-            name: 'player says "sleep"',
+            parent: checkIsNight,
+            child: goToBed,
+            name: '12 Move To Bed when is night',
+            shouldTransition: () => true,
         }),
 
-        
+        new StateTransition({ // 13
+            parent: goToBed,
+            child: goSleep,
+            shouldTransition: () => goToBed.distanceToTarget() < 2,
+            name: "13 Join into bed",
+        }),
+
+        new StateTransition({ // 15
+            parent: goSleep,
+            child: idleState,
+            shouldTransition: () => {
+                if (checkIsNight.isNight() == false && goSleep.isInBed() == true)
+                    return true
+            },
+            name: "15 CLick to Sleep",
+        }),
+
+        new StateTransition({ // 16
+            parent: idleState,
+            child: checkIsNight,
+            shouldTransition: () => checkIsNight.isNight(),
+            name: "16 Check is night",
+        }),
+
     ];
 
     const root = new NestedStateMachine(transitions, printServerStates);
     root.name = "main";
-    
-    bot.on("chat", (username, message) =>
-    {
-        if (message === "hi")
-            transitions[1].trigger();   // parent: idleState,               child: getClosestPlayer,
 
-        if (message === "bye")
-        {
+    bot.on('death', function () {
+        bot.chat('Omg im dead');
+    })
+
+
+    bot.on("chat", (username, message) => {
+        if (message === "hi") {
+            transitions[1].trigger();   // parent: idleState,               child: getClosestPlayer,
+        }
+
+        if (message === "bye") {
             transitions[3].trigger();   // parent: lookAtPlayersState,      child: idleState,
             transitions[6].trigger();   // parent: followPlayer,            child: idleState,
             transitions[9].trigger();   // parent: lookAtFollowTarget,      child: idleState,
@@ -210,21 +221,65 @@ bot.once("spawn", () =>
         if (message === "come")
             transitions[4].trigger();   // parent: lookAtPlayersState,      child: followPlayer,
 
-        if (message === "stay")
-        {
+        if (message === "stay") {
             transitions[5].trigger();   // parent: followPlayer,            child: lookAtPlayersState,
             transitions[10].trigger();  // parent: lookAtFollowTarget,      child: lookAtPlayersState,
         }
 
-        if (message === "sleep")
-        {
-            transitions[12].trigger();  // parent: idleState,               child: goBase
-        }
+    });
 
+    bot.on('time', () => {
+        checkIsNight.check()
     });
 
     const stateMachine = new BotStateMachine(bot, root);
     const webserver = new StateMachineWebserver(bot, stateMachine);
     webserver.startServer();
 });
+
+
+const checkNight = (function () {
+    function checkNight(bot) {
+        this.bot = bot;
+        this.active = false;
+        this.stateName = 'checkIsNight';
+        this.night = false;
+    }
+
+    checkNight.prototype.check = function () {
+        if ((bot.time.timeOfDay >= 0 && bot.time.timeOfDay <= 12040) || (bot.time.timeOfDay >= 23961 && bot.time.timeOfDay <= 24000)) {
+            this.night = false;
+        } else {
+            this.night = true;
+        }
+    }
+    checkNight.prototype.isNight = function () {
+        return this.night;
+    }
+    return checkNight;
+}());
+
+
+const goBedAction = (function () {
+    function goBedAction(bot, bed) {
+        this.bot = bot;
+        this.bed = bed;
+        this.active = false;
+        this.stateName = 'goBedAction';
+        this.actionFinished = false;
+    }
+    goBedAction.prototype.onStateEntered = function () {
+        this.bot.sleep(this.bed, (err) => {
+            if (err) {
+                this.bot.chat(`I can't sleep: ${err.message}`)
+            }
+        })
+        this.actionFinished = true;
+    };
+    goBedAction.prototype.isInBed = function () {
+        return this.actionFinished;
+    }
+    return goBedAction;
+}());
+
 
