f62b7a449a95cf9ddf06e1b9b3375577a8d99b3b
Javier García <javier.garcia@westwing.es>
2020-09-18 21:15:56 +0200

    Optimized code

---
+++ NestedStateModules/baseFunction.js
@@ -0,0 +1,102 @@
+const {
+    StateTransition,
+    EntityFilters,
+    BehaviorIdle,
+    BehaviorFollowEntity,
+    BehaviorLookAtEntity,
+    BehaviorGetClosestEntity,
+    NestedStateMachine
+} = require("mineflayer-statemachine");
+
+
+let baseFunction = (function (bot) {
+    targets = {}
+    const enter = new BehaviorIdle();
+    const exit = new BehaviorIdle();
+    const idleState = new BehaviorIdle();
+
+    const followPlayer = new BehaviorFollowEntity(bot, targets);
+    const getClosestPlayer = new BehaviorGetClosestEntity(bot, targets, EntityFilters().PlayersOnly);
+    const lookAtFollowTarget = new BehaviorLookAtEntity(bot, targets);
+    const lookAtPlayersState = new BehaviorLookAtEntity(bot, targets);
+
+
+    const transitions = [
+        new StateTransition({ // 0
+            parent: enter,
+            child: getClosestPlayer,
+            name: 'enter -> getClosestPlayer',
+            shouldTransition: () => true,
+        }),
+        new StateTransition({ // 1
+            parent: getClosestPlayer,
+            child: lookAtPlayersState,
+            name: 'getClosestPlayer -> lookAtPlayersState',
+            onTransition: () => bot.chat("Hello Master!"),
+            shouldTransition: () => true,
+        }),
+        new StateTransition({ // 2
+            parent: lookAtPlayersState,
+            name: 'Player say: bye',
+            child: exit,
+            shouldTransition: () => false,
+        }),
+        new StateTransition({ // 3
+            parent: lookAtPlayersState,
+            child: followPlayer,
+            name: 'Player say: come',
+            onTransition: () => bot.chat("Yes sr!"),
+            shouldTransition: () => false,
+        }),
+        new StateTransition({ // 4
+            parent: followPlayer,
+            child: lookAtFollowTarget,
+            name: 'The player is too far',
+            shouldTransition: () => followPlayer.distanceToTarget() < 2,
+        }),
+        new StateTransition({ // 5
+            parent: lookAtFollowTarget,
+            child: followPlayer,
+            name: 'The player is too close',
+            shouldTransition: () => lookAtFollowTarget.distanceToTarget() >= 2,
+        }),
+        new StateTransition({ // 6
+            parent: lookAtFollowTarget,
+            child: exit,
+            name: 'Player say: bye',
+        }),
+        new StateTransition({ // 7
+            parent: followPlayer,
+            child: exit,
+            name: 'Player say: bye',
+        }),
+    ];
+
+
+    bot.on("chat", (username, message) => {
+        switch (true) {
+            case (message === "bye"):
+                bot.chat("Bye Master!");
+                transitions[2].trigger();
+                transitions[6].trigger();
+                transitions[7].trigger();
+                break;
+            case (message === "come"):
+                transitions[3].trigger();
+                break;
+        }
+
+
+
+    });
+
+
+    const baseFunction = new NestedStateMachine(transitions, enter, exit);
+    baseFunction.stateName = 'baseFunction'
+    return baseFunction;
+});
+
+
+
+
+module.exports = baseFunction
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const USERNAME = process.env.USER
-const AUTOLOGIN = process.env.AUTOLOGIN
 const SERVER = process.env.SERVER
 const PORT = process.env.PORT
 
@@ -32,123 +31,46 @@ const {
     StateTransition,
     BotStateMachine,
     StateMachineWebserver,
-    EntityFilters,
     BehaviorIdle,
     BehaviorPrintServerStats,
-    BehaviorFollowEntity,
-    BehaviorLookAtEntity,
-    BehaviorGetClosestEntity,
     NestedStateMachine,
     BehaviorMoveTo
 } = require("mineflayer-statemachine");
 const BehaviorIsNight = require("./BehaviorModules/BehaviorIsNight");
-const goSleepFunction = require('./NestedStateModules/goSleepFunction')
+const goSleepFunction = require('./NestedStateModules/goSleepFunction');
+const baseFunction = require('./NestedStateModules/baseFunction');
+
 
 bot.once("spawn", () => {
     const targets = {};
-    const base = {
-        position: {
-            x: -85,
-            y: 64,
-            z: 90
-        }
-    };
+    const base = { position: { x: -85, y: 64, z: 90 } };
 
     const goBase = new BehaviorMoveTo(bot, base);
-
     const isNight = new BehaviorIsNight(bot);
-
     const printServerStates = new BehaviorPrintServerStats(bot);
     const idleState = new BehaviorIdle();
-    const followPlayer = new BehaviorFollowEntity(bot, targets);
-    const getClosestPlayer = new BehaviorGetClosestEntity(bot, targets, EntityFilters().PlayersOnly);
-    const lookAtFollowTarget = new BehaviorLookAtEntity(bot, targets);
-    const lookAtPlayersState = new BehaviorLookAtEntity(bot, targets);
-
     const goSleep = goSleepFunction(bot)
+    const baseCommands = baseFunction(bot)
 
-
-
-    // Create our transitions
     const transitions = [
-
-        new StateTransition({ // 0
+        new StateTransition({
+            parent: idleState,
+            child: baseCommands,
+            name: 'Chat listener',
+        }),
+        new StateTransition({
+            parent: baseCommands,
+            child: idleState,
+            name: 'Finish Command',
+            shouldTransition: () => baseCommands.isFinished(),
+        }),
+        new StateTransition({
             parent: printServerStates,
             child: goBase,
             name: '0 Starts',
             shouldTransition: () => true
         }),
-
-        new StateTransition({ // 1
-            parent: idleState,
-            child: getClosestPlayer,
-            name: '1 player says "hi"',
-            onTransition: () => bot.chat("Hi master!")
-        }),
-
-        new StateTransition({ // 2
-            parent: getClosestPlayer,
-            child: lookAtPlayersState,
-            name: '2 get close entity',
-            shouldTransition: () => true,
-        }),
-
-        new StateTransition({ // 3
-            parent: lookAtPlayersState,
-            child: idleState,
-            name: '3 player says "bye"',
-            onTransition: () => bot.chat("Good bye master!")
-        }),
-
-        new StateTransition({ // 4
-            parent: lookAtPlayersState,
-            child: followPlayer,
-            name: '5 player says "come"',
-            onTransition: () => bot.chat("Yes sr!")
-        }),
-
-        new StateTransition({ // 5
-            parent: followPlayer,
-            child: lookAtPlayersState,
-            name: '5 player says "stay"',
-            onTransition: () => bot.chat("Yes sr!!")
-        }),
-
-        new StateTransition({ //  6
-            parent: followPlayer,
-            child: idleState,
-            name: '6 player says "bye"',
-            onTransition: () => bot.chat("GB sr!")
-        }),
-
-        new StateTransition({ // 7
-            parent: followPlayer,
-            child: lookAtFollowTarget,
-            name: '7 closeToTarget',
-            shouldTransition: () => followPlayer.distanceToTarget() < 2,
-        }),
-
-        new StateTransition({ // 8
-            parent: lookAtFollowTarget,
-            child: followPlayer,
-            name: '8 farFromTarget',
-            shouldTransition: () => lookAtFollowTarget.distanceToTarget() >= 2,
-        }),
-
-        new StateTransition({ // 9
-            parent: lookAtFollowTarget,
-            child: idleState,
-            name: '9 player says "bye"',
-            onTransition: () => bot.chat("goodbye")
-        }),
-
-        new StateTransition({ // 10
-            parent: lookAtFollowTarget,
-            child: lookAtPlayersState,
-            name: '10 player says "stay"',
-        }),
-
-        new StateTransition({ // 11
+        new StateTransition({
             parent: goBase,
             child: idleState,
             name: '11 Near base',
@@ -157,7 +79,6 @@ bot.once("spawn", () => {
                 bot.chat("Im are on base!")
             },
         }),
-
         new StateTransition({
             parent: idleState,
             child: isNight,
@@ -167,7 +88,6 @@ bot.once("spawn", () => {
                 goSleep.targets = isNight.bed;
             },
         }),
-
         new StateTransition({
             parent: isNight,
             child: goSleep,
@@ -175,53 +95,32 @@ bot.once("spawn", () => {
             name: "isNight -> goSleep",
             onTransition: () => {
                 goSleep.targets = isNight.bed
-                //console.log(goSleep)
             },
         }),
-
         new StateTransition({
             parent: goSleep,
             child: idleState,
             shouldTransition: () => goSleep.isFinished(),
             name: "goSleep -> idleState",
         }),
-
-
     ];
-
-    const root = new NestedStateMachine(transitions, printServerStates);
-    root.name = "main";
 
     bot.on('death', function () {
         bot.chat('Omg im dead');
     })
 
-
-    bot.on("chat", (username, message) => {
-        if (message === "hi " + bot.username) {
-            transitions[1].trigger();   // parent: idleState,               child: getClosestPlayer,
-        }
-
-        if (message === "bye") {
-            transitions[3].trigger();   // parent: lookAtPlayersState,      child: idleState,
-            transitions[6].trigger();   // parent: followPlayer,            child: idleState,
-            transitions[9].trigger();   // parent: lookAtFollowTarget,      child: idleState,
-        }
-
-        if (message === "come")
-            transitions[4].trigger();   // parent: lookAtPlayersState,      child: followPlayer,
-
-        if (message === "stay") {
-            transitions[5].trigger();   // parent: followPlayer,            child: lookAtPlayersState,
-            transitions[10].trigger();  // parent: lookAtFollowTarget,      child: lookAtPlayersState,
-        }
-
-    });
-
     bot.on('time', () => {
         isNight.check()
     });
 
+    bot.on("chat", (username, message) => {
+        if (message === "hi " + bot.username) {
+            transitions[0].trigger();
+        }
+    });
+
+    const root = new NestedStateMachine(transitions, printServerStates);
+    root.name = "main";
     const stateMachine = new BotStateMachine(bot, root);
     const webserver = new StateMachineWebserver(bot, stateMachine);
     webserver.startServer();
