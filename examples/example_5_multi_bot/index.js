7c4c621a7c847cf9c30e8657aefd9fa0129571c6
Javier García <javier.garcia@westwing.es>
2020-09-19 23:31:16 +0200

    Checkpoint multi bot

--- examples/example_5_multi_bot/index.js
+++ examples/example_5_multi_bot/index.js
@@ -1,25 +1,48 @@
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
 const config = require('../../config')
-
 
 let i = 0
 
-function next() {
-    if (i < 7) {
+function Guards() {
+    if (i < 3) {
         i++;
         setTimeout(() => {
-            console.log(`Guard${i}`);
             createBot(`Guard${i}`)
-            next()
-        }, 1000)
+            Guards()
+        }, 500)
+    } else {
+        i = 0;
+        Archers();
     }
 }
-next()
+
+
+
+function Archers() {
+    if (i < 3) {
+        i++;
+        setTimeout(() => {
+            createBot(`Archer${i}`)
+            Archers()
+        }, 500)
+    }
+}
+Guards()
 
 function createBot(name) {
-    mineflayer.createBot({
-        host: config.server,
-        port: config.port,
-        username: name
-    })
+    return new Promise((resolve, reject) => {
+        let bot = mineflayer.createBot({
+            host: config.server,
+            port: config.port,
+            username: name
+        })
+
+        bot.on('death', function() {
+            bot.chat('Omg im dead');
+        })
+
+        resolve(name)
+    }).then(username => {
+        console.log("Bot loged", username)
+    });
 }
