6daca16d23662d941a81499780b75d9f3efa12be
Javier García <javier.garcia@westwing.es>
2020-09-19 09:57:12 +0200

    Added users

---
+++ examples/nodejs/serverchat/components/user/network.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const response = require('../../network/response')
+const controller = require('./controller')
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    const filterUsers = req.query.user || null;
+    controller.getUsers(filterUsers)
+        .then((result) => {
+            response.success(req, res, result)
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, '[user network] Error desconocido')
+        });
+})
+
+router.get('/:id', function (req, res) {
+    controller.getUser(req.params.id)
+        .then((result) => {
+            response.success(req, res, result)
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, '[user network] Error desconocido')
+        });
+})
+
+router.post('/', function (req, res) {
+    controller.addUser(req.body.user)
+        .then((result) => {
+            response.success(req, res, result)
+        })
+        .catch(e => {
+            response.error(req, res, 'Informacion Invalida', 500, '[user network] No se ha insertado usuario o contraseña')
+        });
+})
+
+router.patch('/:id', function (req, res) {
+    controller.updateUser(req.body.user, req.params.id)
+        .then(() => {
+            response.success(req, res, `Usuario ${req.params.id} eliminado`)
+        })
+        .catch(e => {
+            response.error(req, res, 'Informacion Invalida', 500, '[user network] No se ha insertado usuario o contraseña o id')
+        });
+})
+
+router.delete('/:id', function (req, res) {
+    controller.deleteUser(req.params.id)
+        .then(() => {
+            response.success(req, res, `Usuario ${req.params.id} eliminado`)
+        })
+        .catch(e => {
+            response.error(req, res, 'Informacion Invalida', 500, '[user network] No se ha insertado id')
+        });
+})
+
+
+module.exports = router;
---
+++ examples/nodejs/serverchat/components/user/store.js
@@ -0,0 +1,70 @@
+
+const MongoLib = require('../../network/MongoLib');
+const db = new MongoLib;
+// const Model = require('./model')
+
+db.connect();
+
+async function addUser(User) {
+    return db.create('User', User)
+        .then(result => {
+            return result;
+        })
+        .catch(err => {
+            return err;
+        });
+}
+
+function getUsers(filterUser) {
+    let filter = {};
+    if (filterUser !== null) {
+        filter = { user: filterUser }
+    }
+
+    return db.getAll('User', filter)
+        .then(result => {
+            return result
+        })
+        .catch(err => {
+            return err;
+        })
+}
+
+function getUser(id) {
+    return db.get('User', id)
+        .then(result => {
+            return result
+        })
+        .catch(err => {
+            return err;
+        })
+}
+
+
+function updateUser(id, message) {
+    return db.update('User', id, message)
+        .then(result => {
+            return result
+        })
+        .catch(err => {
+            return err;
+        })
+}
+
+function deleteUser(id) {
+    return db.delete('User', id)
+        .then(result => {
+            return result
+        })
+        .catch(err => {
+            return err;
+        })
+}
+
+module.exports = {
+    get: getUser,
+    add: addUser,
+    list: getUsers,
+    update: updateUser,
+    delete: deleteUser
+}
