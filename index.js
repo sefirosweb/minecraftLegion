c1e4e5d1ee41c410139323b0704bccb07212f90c
Javier García <javier.garcia@westwing.es>
2020-09-19 22:57:04 +0200

    checkpoint

--- index.js
+++ index.js
@@ -36,117 +36,117 @@ bot.once("spawn", () => {
 
 })
 
-/*
-    bot.loadPlugin(require('mineflayer-pathfinder').pathfinder);
 
-    const {
-        StateTransition,
-        BotStateMachine,
-        StateMachineWebserver,
-        BehaviorIdle,
-        BehaviorPrintServerStats,
-        NestedStateMachine,
-        BehaviorMoveTo
-    } = require("mineflayer-statemachine");
-    const BehaviorIsNight = require("./BehaviorModules/BehaviorIsNight");
-    const goSleepFunction = require('./NestedStateModules/goSleepFunction');
-    const baseFunction = require('./NestedStateModules/baseFunction');
+bot.loadPlugin(require('mineflayer-pathfinder').pathfinder);
+
+const {
+    StateTransition,
+    BotStateMachine,
+    StateMachineWebserver,
+    BehaviorIdle,
+    BehaviorPrintServerStats,
+    NestedStateMachine,
+    BehaviorMoveTo
+} = require("mineflayer-statemachine");
+const BehaviorIsNight = require("./BehaviorModules/BehaviorIsNight");
+const goSleepFunction = require('./NestedStateModules/goSleepFunction');
+const baseFunction = require('./NestedStateModules/baseFunction');
 
 
-    bot.once("spawn", () => {
-        const targets = {};
-        const base = { position: { x: -60, y: 80, z: 125 } };
+bot.once("spawn", () => {
+    const targets = {};
+    const base = { position: { x: -60, y: 80, z: 125 } };
 
-        const goBase = new BehaviorMoveTo(bot, base);
-        const isNight = new BehaviorIsNight(bot);
-        const printServerStates = new BehaviorPrintServerStats(bot);
-        const idleState = new BehaviorIdle();
-        const goSleep = goSleepFunction(bot)
-        const baseCommands = baseFunction(bot)
+    const goBase = new BehaviorMoveTo(bot, base);
+    const isNight = new BehaviorIsNight(bot);
+    const printServerStates = new BehaviorPrintServerStats(bot);
+    const idleState = new BehaviorIdle();
+    const goSleep = goSleepFunction(bot)
+    const baseCommands = baseFunction(bot)
 
-        const transitions = [
-            new StateTransition({ // 0
-                parent: idleState,
-                child: baseCommands,
-                name: 'Chat listener',
-            }),
-            new StateTransition({ // 1
-                parent: baseCommands,
-                child: printServerStates,
-                shouldTransition: () => false,
-                name: "Restart when players die",
-            }),
-            new StateTransition({
-                parent: baseCommands,
-                child: idleState,
-                name: 'Finish Command',
-                shouldTransition: () => baseCommands.isFinished(),
-            }),
-            new StateTransition({
-                parent: printServerStates,
-                //child: goBase,
-                child: idleState,
-                name: '0 Starts',
-                shouldTransition: () => true
-            }),
-            new StateTransition({
-                parent: goBase,
-                child: idleState,
-                name: '11 Near base',
-                shouldTransition: () => goBase.distanceToTarget() < 2,
-                onTransition: () => {
-                    bot.chat("Im are on base!")
-                },
-            }),
-            new StateTransition({
-                parent: idleState,
-                child: isNight,
-                shouldTransition: () => isNight.getIsNight() && isNight.getBed() !== false,
-                name: "idleState -> isNight",
-                onTransition: () => {
-                    goSleep.targets = isNight.bed;
-                },
-            }),
-            new StateTransition({
-                parent: isNight,
-                child: goSleep,
-                shouldTransition: () => true,
-                name: "isNight -> goSleep",
-                onTransition: () => {
-                    goSleep.targets = isNight.bed
-                },
-            }),
-            new StateTransition({
-                parent: goSleep,
-                child: idleState,
-                shouldTransition: () => goSleep.isFinished(),
-                name: "goSleep -> idleState",
-            }),
+    const transitions = [
+        new StateTransition({ // 0
+            parent: idleState,
+            child: baseCommands,
+            name: 'Chat listener',
+        }),
+        new StateTransition({ // 1
+            parent: baseCommands,
+            child: printServerStates,
+            shouldTransition: () => false,
+            name: "Restart when players die",
+        }),
+        new StateTransition({
+            parent: baseCommands,
+            child: idleState,
+            name: 'Finish Command',
+            shouldTransition: () => baseCommands.isFinished(),
+        }),
+        new StateTransition({
+            parent: printServerStates,
+            //child: goBase,
+            child: idleState,
+            name: '0 Starts',
+            shouldTransition: () => true
+        }),
+        new StateTransition({
+            parent: goBase,
+            child: idleState,
+            name: '11 Near base',
+            shouldTransition: () => goBase.distanceToTarget() < 2,
+            onTransition: () => {
+                bot.chat("Im are on base!")
+            },
+        }),
+        new StateTransition({
+            parent: idleState,
+            child: isNight,
+            shouldTransition: () => isNight.getIsNight() && isNight.getBed() !== false,
+            name: "idleState -> isNight",
+            onTransition: () => {
+                goSleep.targets = isNight.bed;
+            },
+        }),
+        new StateTransition({
+            parent: isNight,
+            child: goSleep,
+            shouldTransition: () => true,
+            name: "isNight -> goSleep",
+            onTransition: () => {
+                goSleep.targets = isNight.bed
+            },
+        }),
+        new StateTransition({
+            parent: goSleep,
+            child: idleState,
+            shouldTransition: () => goSleep.isFinished(),
+            name: "goSleep -> idleState",
+        }),
 
-        ];
+    ];
 
-        bot.on('death', function() {
-            bot.chat('Omg im dead');
-            transitions[1].trigger();
-        })
+    bot.on('death', function() {
+        bot.chat('Omg im dead');
+        transitions[1].trigger();
+    })
 
-        bot.on('time', () => {
-            isNight.check()
-        });
-
-        bot.on("chat", (username, message) => {
-            if (message === "hi " + bot.username) {
-                transitions[0].trigger();
-            }
-        });
-
-        const root = new NestedStateMachine(transitions, printServerStates);
-        root.name = "main";
-        const stateMachine = new BotStateMachine(bot, root);
-        const webserver = new StateMachineWebserver(bot, stateMachine);
-        webserver.startServer();
+    bot.on('time', () => {
+        isNight.check()
     });
-    */
+
+    bot.on("chat", (username, message) => {
+        if (message === "hi " + bot.username) {
+            transitions[0].trigger();
+        }
+    });
+
+    const root = new NestedStateMachine(transitions, printServerStates);
+    root.name = "main";
+    const stateMachine = new BotStateMachine(bot, root);
+    const webserver = new StateMachineWebserver(bot, stateMachine);
+    webserver.startServer();
+});
+
 
 /*
 const mineflayer = require('mineflayer')
