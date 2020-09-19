ed3ce467d0efd8e3f0ba62628d49f95128f34ac8
Javier García <javier.garcia@westwing.es>
2020-09-19 22:50:51 +0200

    bug check point

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 require('dotenv').config()
 const config = require('./config')
 
@@ -25,18 +26,14 @@ const mineflayerViewer = require('prismarine-viewer').mineflayer
 bot.once("spawn", () => {
     bot.chat('Im in!')
     inventoryViewer(bot)
-
-    /*
-        mineflayerViewer(bot, { port: 4001 })
-        const path = [bot.entity.position.clone()]
-        console.log("port", port)
-        bot.on('move', () => {
-            if (path[path.length - 1].distanceTo(bot.entity.position) > 1) {
-                path.push(bot.entity.position.clone())
-                bot.viewer.drawLine('path', path)
-            }
-        })
-    */
+    mineflayerViewer(bot, { port: 4000 })
+    const path = [bot.entity.position.clone()]
+    bot.on('move', () => {
+        if (path[path.length - 1].distanceTo(bot.entity.position) > 1) {
+            path.push(bot.entity.position.clone())
+            bot.viewer.drawLine('path', path)
+        }
+    })
 
 })
 
@@ -150,3 +147,26 @@ bot.once("spawn", () => {
     const webserver = new StateMachineWebserver(bot, stateMachine);
     webserver.startServer();
 });
+*/
+
+const mineflayer = require('mineflayer')
+const mineflayerViewer = require('prismarine-viewer').mineflayer
+
+
+const bot = mineflayer.createBot({
+    username: 'Guard1',
+    port: 65318
+})
+
+bot.once('spawn', () => {
+    mineflayerViewer(bot, { port: 4000 }) // Start the viewing server on port 3000
+
+    // Draw the path followed by the bot
+    const path = [bot.entity.position.clone()]
+    bot.on('move', () => {
+        if (path[path.length - 1].distanceTo(bot.entity.position) > 1) {
+            path.push(bot.entity.position.clone())
+            bot.viewer.drawLine('path', path)
+        }
+    })
+})
