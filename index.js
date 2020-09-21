b632be153ca4d72ab78a5548aab55fec47c2ca74
Javier García <javier.garcia@westwing.es>
2020-09-21 23:01:38 +0200

    Optimized code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const BehaviorIsNight = require("./BehaviorModules/BehaviorIsNight");
 const BehaviorGetPlayer = require("./BehaviorModules/BehaviorGetPlayer");
 
 const goSleepFunction = require('./NestedStateModules/goSleepFunction');
-// const baseFunction = require('./NestedStateModules/baseFunction');
+const baseFunction = require('./NestedStateModules/baseFunction');
 
 // const { start } = require('repl');
 
@@ -55,9 +55,6 @@ function startBots() {
 };
 startBots();
 
-let playerName = {};
-
-
 function createNewBot(botName, portBotStateMachine = null, portPrismarineViewer = null, portInventory = null) {
     const bot = mineflayer.createBot({
         username: botName,
@@ -86,9 +83,7 @@ function createNewBot(botName, portBotStateMachine = null, portPrismarineViewer 
 
     bot.once("spawn", () => {
         const targets = {};
-
         const base = { position: { x: -81, y: 68, z: 96 } };
-
         const goBase = new BehaviorMoveTo(bot, base);
         const isNight = new BehaviorIsNight(bot, targets);
         const idleState = new BehaviorIdle(targets);
@@ -181,75 +176,4 @@ function createNewBot(botName, portBotStateMachine = null, portPrismarineViewer 
         }
     });
 
-}
-
-
-function baseFunction(bot, targets) {
-
-    const enter = new BehaviorIdle(targets);
-    const exit = new BehaviorIdle(targets);
-
-    const followPlayer = new BehaviorFollowEntity(bot, targets);
-    const lookAtFollowTarget = new BehaviorLookAtEntity(bot, targets);
-    const lookAtPlayersState = new BehaviorLookAtEntity(bot, targets);
-
-    const transitions = [
-        new StateTransition({
-            parent: lookAtPlayersState,
-            child: exit,
-            name: 'Player say: bye',
-        }),
-        new StateTransition({
-            parent: followPlayer,
-            child: exit,
-            name: 'Player say: bye',
-        }),
-        new StateTransition({
-            parent: lookAtFollowTarget,
-            child: exit,
-            name: 'Player say: bye',
-        }),
-        new StateTransition({
-            parent: lookAtPlayersState,
-            child: followPlayer,
-            name: 'Player say: come',
-            onTransition: () => bot.chat("Yes sr!"),
-        }),
-        new StateTransition({
-            parent: followPlayer,
-            child: lookAtFollowTarget,
-            name: 'The player is too far',
-            shouldTransition: () => followPlayer.distanceToTarget() < 2,
-        }),
-        new StateTransition({
-            parent: lookAtFollowTarget,
-            child: followPlayer,
-            name: 'The player is too close',
-            shouldTransition: () => lookAtFollowTarget.distanceToTarget() >= 2,
-        }),
-        new StateTransition({
-            parent: enter,
-            child: lookAtPlayersState,
-            name: 'Enter to nested',
-            shouldTransition: () => true,
-        }),
-    ];
-
-    bot.on("chat", (username, message) => {
-        switch (true) {
-            case (message === "bye"):
-                bot.chat("Bye Master!");
-                transitions[0].trigger();
-                transitions[1].trigger();
-                transitions[2].trigger();
-                break;
-            case (message === "come"):
-                transitions[3].trigger();
-                break;
-        }
-    });
-
-    const baseFunction = new NestedStateMachine(transitions, enter, exit);
-    baseFunction.stateName = 'baseFunction'
-    return baseFunction;
 }
