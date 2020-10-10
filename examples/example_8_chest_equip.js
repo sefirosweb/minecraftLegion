33f40b704de5064306887fc9119274a92e4edc69
Javier García <javier.garcia@westwing.es>
2020-10-10 22:31:58 +0200

    Adding tests

---
+++ examples/example_8_chest_equip.js
@@ -0,0 +1,22 @@
+const mineflayer = require('mineflayer')
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+    console.log('Usage : node chest.js <host> <port> [<name>] [<password>]')
+    process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    username: process.argv[4] ? process.argv[4] : 'chest',
+    password: process.argv[5]
+})
+
+let mcData
+bot.once('inject_allowed', () => {
+    mcData = require('minecraft-data')(bot.version)
+})
+
+bot.on('experience', () => {
+    bot.chat(`I am level ${bot.experience.level}`)
+})
