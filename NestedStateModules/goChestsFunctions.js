efeb99367e745c9512bfa5ef4a1e6e950cf9bddf
sefirosweb <sefirosweb@gmail.com>
2021-02-13 17:32:18 +0100

    Added new sub function to go chests and deposit

---
+++ NestedStateModules/goChestsFunctions.js
@@ -0,0 +1,149 @@
+const {
+  StateTransition,
+  BehaviorIdle,
+  BehaviorMoveTo,
+  NestedStateMachine
+} = require('mineflayer-statemachine')
+
+const BehaviorLoadConfig = require('./../BehaviorModules/BehaviorLoadConfig')
+const BehaviorWithdrawItemChest = require('./../BehaviorModules/BehaviorWithdrawItemChest')
+const BehaviorDepositChest = require('./../BehaviorModules/BehaviorDepositChest')
+const BehaviorCheckItemsInInventory = require('./../BehaviorModules/BehaviorCheckItemsInInventory')
+
+function goChestsFunction (bot, targets) {
+  const start = new BehaviorIdle()
+  start.stateName = 'Start'
+  start.x = 125
+  start.y = 113
+
+  const exit = new BehaviorIdle()
+  exit.stateName = 'Exit'
+  exit.x = 725
+  exit.y = 113
+
+  const nextCheck = new BehaviorIdle()
+  nextCheck.stateName = 'Next Check'
+  nextCheck.x = 525
+  nextCheck.y = 113
+
+  const loadConfig = new BehaviorLoadConfig(bot, targets)
+  loadConfig.stateName = 'Load Bot Config'
+  loadConfig.x = 325
+  loadConfig.y = 113
+
+  const checkItemsInInventory = new BehaviorCheckItemsInInventory(bot, targets)
+  checkItemsInInventory.stateName = 'Checks Items in Inventory'
+  checkItemsInInventory.x = 525
+  checkItemsInInventory.y = 250
+
+  const goChest = new BehaviorMoveTo(bot, targets)
+  goChest.stateName = 'Go chest'
+  goChest.x = 525
+  goChest.y = 413
+
+  const withdrawItems = new BehaviorWithdrawItemChest(bot, targets)
+  withdrawItems.stateName = 'Withdraw Items'
+  withdrawItems.x = 325
+  withdrawItems.y = 250
+
+  const depositItems = new BehaviorDepositChest(bot, targets)
+  depositItems.stateName = 'Deposit Items'
+  depositItems.x = 725
+  depositItems.y = 250
+
+  let chests = []
+  let chestIndex = 0
+
+  const transitions = [
+    new StateTransition({
+      parent: start,
+      child: loadConfig,
+      name: 'start -> loadConfig',
+      shouldTransition: () => true
+    }),
+
+    new StateTransition({
+      parent: loadConfig,
+      child: nextCheck,
+      name: 'loadConfig -> checkItemsInInventory',
+      onTransition: () => {
+        chestIndex = 0
+        chests = loadConfig.getAllChests()
+      },
+      shouldTransition: () => true
+    }),
+
+    new StateTransition({
+      parent: nextCheck,
+      child: exit,
+      name: 'All chest checked',
+      shouldTransition: () => chestIndex === (chests.length)
+    }),
+
+    new StateTransition({
+      parent: nextCheck,
+      child: checkItemsInInventory,
+      name: 'All chest checked',
+      onTransition: () => {
+        checkItemsInInventory.setItemsToCheck(chests[chestIndex].items)
+        checkItemsInInventory.setIsDeposit(chests[chestIndex].type)
+      },
+      shouldTransition: () => chestIndex < (chests.length)
+    }),
+
+    new StateTransition({
+      parent: checkItemsInInventory,
+      child: nextCheck,
+      name: 'Check next chest',
+      onTransition: () => chestIndex++,
+      shouldTransition: () => checkItemsInInventory.isFinished() && targets.items.length === 0
+    }),
+
+    new StateTransition({
+      parent: checkItemsInInventory,
+      child: goChest,
+      name: 'Go to chest',
+      onTransition: () => {
+        console.log(chests[chestIndex])
+        targets.position = chests[chestIndex].position
+      },
+      shouldTransition: () => checkItemsInInventory.isFinished() && targets.items.length > 0
+    }),
+
+    new StateTransition({
+      parent: goChest,
+      child: withdrawItems,
+      name: 'goChest -> withdrawItems',
+      shouldTransition: () => goChest.isFinished() && !bot.pathfinder.isMining() && chests[chestIndex].type === 'withdraw'
+    }),
+
+    new StateTransition({
+      parent: goChest,
+      child: depositItems,
+      name: 'goChest -> depositItems',
+      shouldTransition: () => goChest.isFinished() && !bot.pathfinder.isMining() && chests[chestIndex].type === 'deposit'
+    }),
+
+    new StateTransition({
+      parent: withdrawItems,
+      child: nextCheck,
+      name: 'withdrawItems -> checkItemsInInventory',
+      onTransition: () => chestIndex++,
+      shouldTransition: () => withdrawItems.isFinished()
+    }),
+
+    new StateTransition({
+      parent: depositItems,
+      child: nextCheck,
+      name: 'withdrawItems -> checkItemsInInventory',
+      onTransition: () => chestIndex++,
+      shouldTransition: () => depositItems.isFinished()
+    })
+  ]
+
+  const goChestsFunction = new NestedStateMachine(transitions, start, exit)
+  goChestsFunction.stateName = 'Go Chests'
+  return goChestsFunction
+}
+
+module.exports = goChestsFunction
