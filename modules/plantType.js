b81cda8d2a509134cbc1bf016c86eeeab7630acd
sefirosweb <sefirosweb@gmail.com>
2021-03-06 21:23:59 +0100

    Added beetroot_seeds

--- modules/plantType.js
+++ modules/plantType.js
@@ -1,6 +1,7 @@
 module.exports = {
   carrot: { type: 'normal', seed: 'carrot' },
   potato: { type: 'normal', seed: 'potato' },
+  beetroot: { type: 'normal', seed: 'beetroot_seeds' },
   wheat: { type: 'normal', seed: 'wheat_seeds' },
   melon: { type: 'melon', seed: 'melon_seeds' },
   pumpkin: { type: 'melon', seed: 'pumpkin_seeds' }
