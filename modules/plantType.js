28cf8c196e0428251f35a79b1fdc54d1fa3027b9
sefirosweb <sefirosweb@gmail.com>
2021-06-26 23:05:00 +0200

    changed plant name melon

--- modules/plantType.js
+++ modules/plantType.js
@@ -4,7 +4,7 @@ module.exports = {
   beetroot: { plantName: 'beetroots', type: 'normal', seed: 'beetroot_seeds', age: 3 },
   wheat: { plantName: 'wheat', type: 'normal', seed: 'wheat_seeds', age: 7 },
   sweet_berries: { plantName: 'sweet_berry_bush', type: 'sweet_berries', seed: 'sweet_berries', age: 3 },
-  melon: { plantName: 'melon', type: 'melon', seed: 'melon_seeds', age: null },
+  melon: { plantName: 'melon_slice', type: 'melon', seed: 'melon_seeds', age: null },
   pumpkin: { plantName: 'pumpkin', type: 'melon', seed: 'pumpkin_seeds', age: null },
   oak_sapling: { plantName: 'oak_sapling', type: 'tree', seed: 'oak_sapling', age: null },
   birch_sapling: { plantName: 'birch_sapling', type: 'tree', seed: 'birch_sapling', age: null },
