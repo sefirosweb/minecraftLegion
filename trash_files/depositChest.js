10c8f4e9099480820e8fe756b8e52d75e85b0000
Sefirosweb <sefirosweb@gmail.com>
2021-08-08 14:19:07 +0200

    Change requeriments for start

--- trash_files/depositChest.js
+++ trash_files/depositChest.js
@@ -1,14 +1,14 @@
 const mineflayer = require('mineflayer')
 
-if (process.argv.length < 4 || process.argv.length > 6) {
+if (process.argv.length < 3 || process.argv.length > 6) {
   console.log('Usage : node chest.js <host> <port> [<name>] [<password>]')
   process.exit(1)
 }
 
 const bot = mineflayer.createBot({
   host: process.argv[2],
-  port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'chest',
+  port: process.argv[3] ? parseInt(process.argv[3]) : 25565,
+  username: process.argv[4] ? process.argv[4] : 'ChestBot',
   password: process.argv[5]
 })
 
