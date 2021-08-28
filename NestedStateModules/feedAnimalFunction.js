2a2b915b1ac4c761fb017b1ec4e73588a64c4b39
Sefirosweb <sefirosweb@gmail.com>
2021-08-28 23:52:11 +0200

    Added equip function

--- NestedStateModules/feedAnimalFunction.js
+++ NestedStateModules/feedAnimalFunction.js
@@ -7,9 +7,11 @@ const {
 } = require('mineflayer-statemachine')
 
 const BehaviorLoadConfig = require('@BehaviorModules/BehaviorLoadConfig')
+const BehaviorEquip = require('@BehaviorModules/BehaviorEquip')
 
 function feedAnimalFunction (bot, targets) {
   targets.breededAnimals = []
+  const mcData = require('minecraft-data')(bot.version)
 
   const start = new BehaviorIdle(targets)
   start.stateName = 'Start'
@@ -26,6 +28,11 @@ function feedAnimalFunction (bot, targets) {
   exit.x = 325
   exit.y = 613
 
+  const equip = new BehaviorEquip(bot, targets)
+  equip.stateName = 'Equip'
+  equip.x = 11
+  equip.y = 11
+
   const followAnimal = new BehaviorFollowEntity(bot, targets)
   followAnimal.stateName = 'Follow Animal'
   followAnimal.x = 100
@@ -34,17 +41,36 @@ function feedAnimalFunction (bot, targets) {
   const transitions = [
     new StateTransition({
       parent: start,
-      child: followAnimal,
+      child: equip,
       onTransition: () => {
-        console.log(targets.entity)
+        // Select item to give food
+        targets.item = mcData.itemsByName.wheat
       },
       shouldTransition: () => true
     }),
 
     new StateTransition({
+      parent: equip,
+      child: followAnimal,
+      shouldTransition: () => equip.isFinished() && equip.isWasEquipped()
+    }),
+
+    new StateTransition({
+      parent: equip,
+      child: exit,
+      shouldTransition: () => equip.isFinished() && !equip.isWasEquipped()
+    }),
+
+    new StateTransition({
       parent: followAnimal,
       child: exit,
-      shouldTransition: () => !targets.entity || (followAnimal.distanceToTarget() < 2 && targets.entity.isValid)
+      shouldTransition: () => !targets.entity || !targets.entity.isValid
+    }),
+
+    new StateTransition({
+      parent: followAnimal,
+      child: exit,
+      shouldTransition: () => targets.entity && followAnimal.distanceToTarget() < 2 && targets.entity.isValid
     })
 
   ]
