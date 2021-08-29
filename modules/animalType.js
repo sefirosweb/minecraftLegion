5b277697dfbce8c90dca6e56f6f2e8d8c9450623
Sefirosweb <sefirosweb@gmail.com>
2021-08-29 14:57:12 +0200

    Added basic info for feed animals

---
+++ modules/animalType.js
@@ -0,0 +1,44 @@
+module.exports = {
+  sheep: {
+    foods: ['wheat']
+  },
+  cow: {
+    foods: ['wheat']
+  },
+  wolf: {
+    food: ['beef', 'chicken', 'rabbit', 'porkchop']
+  },
+  chicken: {
+    food: ['wheat_seeds']
+  },
+  cat: {
+    food: ['cod', 'salmon']
+  },
+  horse: {
+    food: ['golden_carrot', 'golden_apple']
+  },
+  donkey: {
+    food: ['golden_carrot', 'golden_apple']
+  },
+  llama: {
+    food: ['hay_block']
+  },
+  pig: {
+    food: ['carrot', 'potato', 'beetroot']
+  },
+  rabbit: {
+    food: ['carrot', 'golden_carrot', 'dandelion']
+  },
+  turtles: {
+    food: ['seagrass']
+  },
+  panda: {
+    food: ['bamboo']
+  },
+  fox: {
+    food: ['sweet_berries']
+  },
+  bee: {
+    food: ['dandelion', 'poppy', 'blue_orchid', 'allium', 'azure_bluet', 'orange_tulip', 'pink_tulip', 'red_tulip', 'white_tulip', 'cornflower', 'lily_of_the_valley', 'oxeye_daisy']
+  }
+}
