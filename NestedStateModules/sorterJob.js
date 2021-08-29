32ffec1d2b7d315b192a8c763c952c8977c56917
Sefirosweb <sefirosweb@gmail.com>
2021-08-29 21:53:39 +0200

    adding sorter

---
+++ NestedStateModules/sorterJob.js
@@ -0,0 +1,33 @@
+const {
+  StateTransition,
+  BehaviorIdle,
+  NestedStateMachine
+} = require('mineflayer-statemachine')
+
+function sorterJob (bot, targets) {
+  const start = new BehaviorIdle(targets)
+  start.stateName = 'Start'
+  start.x = 125
+  start.y = 113
+
+  const idl = new BehaviorIdle(targets)
+  idl.stateName = 'idl'
+
+  const transitions = [
+    new StateTransition({
+      parent: start,
+      child: idl,
+      onTransition: () => {
+        targets.entity = undefined
+      },
+      shouldTransition: () => true
+    })
+
+  ]
+
+  const sorterJob = new NestedStateMachine(transitions, start)
+  sorterJob.stateName = 'Sorter Job'
+  return sorterJob
+}
+
+module.exports = sorterJob
