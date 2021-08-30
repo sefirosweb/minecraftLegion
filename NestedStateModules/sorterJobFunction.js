7c792455433b4ee792ae0c3a324e3cad6710ca15
Sefirosweb <sefirosweb@gmail.com>
2021-08-30 22:43:44 +0200

    Added find chests

--- NestedStateModules/sorterJobFunction.js
+++ NestedStateModules/sorterJobFunction.js
@@ -1,22 +1,42 @@
 const {
   StateTransition,
   BehaviorIdle,
+  BehaviorMoveTo,
   NestedStateMachine
 } = require('mineflayer-statemachine')
 
 function sorterJobFunction (bot, targets) {
+  const { findChests } = require('@modules/inventoryModule')(bot)
+
   const start = new BehaviorIdle(targets)
   start.stateName = 'Start'
   start.x = 125
   start.y = 113
 
-  const idl = new BehaviorIdle(targets)
-  idl.stateName = 'idl'
+  const dd = new BehaviorIdle(targets)
+
+  const goChest = new BehaviorMoveTo(bot, targets)
+  goChest.stateName = 'Go chest'
+  goChest.x = 525
+  goChest.y = 413
+  goChest.movements = targets.movements
 
   const transitions = [
     new StateTransition({
       parent: start,
-      child: idl,
+      child: goChest,
+      onTransition: () => {
+        targets.chests = findChests({
+          count: 9999,
+          maxDistance: 40
+        })
+      },
+      shouldTransition: () => true
+    }),
+
+    new StateTransition({
+      parent: goChest,
+      child: dd,
       onTransition: () => {
         targets.entity = undefined
       },
