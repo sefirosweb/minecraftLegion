87d1a0e3de5887087fbd9c660fd10045e660f687
Sefirosweb <sefirosweb@gmail.com>
2021-09-01 22:31:22 +0200

    Added logic for sort chests

--- NestedStateModules/sorterJobFunction.js
+++ NestedStateModules/sorterJobFunction.js
@@ -119,7 +119,69 @@ function sorterJobFunction (bot, targets) {
       parent: calculateSort,
       child: d,
       onTransition: () => {
-        // targets.chests.map(chest => console.log(chest.slots))
+        const allChests = targets.chests.map(chest => chest.slots)
+
+        const allItems = allChests.reduce((items, chest) => {
+          chest.forEach(item => {
+            if (item === null) return
+            const indexItem = items.findIndex(i => i.type === item.type)
+            if (indexItem >= 0) {
+              items[indexItem].count += item.count
+            } else {
+              items.push(item)
+            }
+          })
+          return items
+        }, []).sort((a, b) => a.type - b.type)
+
+        let chestIndex = 0
+        const newChestSort = []
+        let newSlots = []
+
+        allItems.forEach(item => {
+          let count = item.count
+
+          if (newSlots.length === 0) {
+            while (count > 0) {
+              const itemToDeposit = { ...item }
+              itemToDeposit.count = count > item.stackSize ? item.stackSize : count
+              count -= itemToDeposit.count
+              newSlots.push(itemToDeposit)
+
+              if (newSlots.length === allChests[chestIndex].length) {
+                newChestSort.push(newSlots)
+                chestIndex++
+                newSlots = []
+              }
+            }
+          } else {
+            const slotNeeded = Math.ceil(count / item.stackSize)
+            const freeSlots = allChests[chestIndex].length - newSlots.length
+
+            if (slotNeeded > freeSlots) {
+              newChestSort.push(newSlots)
+              chestIndex++
+              newSlots = []
+            }
+
+            while (count > 0) {
+              const itemToDeposit = { ...item }
+              itemToDeposit.count = count > item.stackSize ? item.stackSize : count
+              count -= itemToDeposit.count
+              newSlots.push(itemToDeposit)
+
+              if (newSlots.length === allChests[chestIndex].length) {
+                newChestSort.push(newSlots)
+                chestIndex++
+                newSlots = []
+              }
+            }
+          }
+        })
+
+        newChestSort.push(newSlots)
+
+        console.log(newChestSort)
       },
       shouldTransition: () => true
     }),
