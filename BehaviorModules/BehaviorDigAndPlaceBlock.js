674a832e83f8ec549e12eb1689a344aa9488959b
Sefirosweb <sefirosweb@gmail.com>
2021-09-28 23:19:07 +0200

    Adding new behavior (pending)

---
+++ BehaviorModules/BehaviorDigAndPlaceBlock.js
@@ -0,0 +1,28 @@
+const botWebsocket = require('@modules/botWebsocket')
+module.exports = class template {
+  constructor (bot, targets) {
+    this.bot = bot
+    this.targets = targets
+    this.stateName = 'BehaviorDigAndPlaceBlock'
+
+    this.digBlockModule = require('@modules/digBlockModule')(bot)
+
+    this.isEndFinished = false
+  }
+
+  isFinished () {
+    return this.isEndFinished
+  }
+
+  onStateEntered () {
+    this.isEndFinished = false
+    this.digBlockModule.digBlock(this.targets.position)
+      .then(() => {
+        this.isEndFinished = true
+      })
+      .catch((err) => {
+        console.log(err)
+        botWebsocket.log(err)
+      })
+  }
+}
