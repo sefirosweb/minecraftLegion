b5c45c34f34555280c09bf4e33da07b49a91126d
Javier García <javier.garcia@decowood.es>
2022-01-17 19:33:52 +0100

    refact

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-require('module-alias/register')
-const { autoRestart } = require('@config')
-const cp = require('child_process')
-const path = require('path')
+require("module-alias/register");
 
-function startBot (botName, password) {
-  const command = 'node ' + path.join(__dirname, 'start_bot') + ' ' + botName
+const { autoRestart } = require("@config");
+const cp = require("child_process");
+const path = require("path");
+
+function startBot(botName, password) {
+  const command = "node " + path.join(__dirname, "start_bot") + " " + botName;
   cp.exec(command, (err, stdout, stderr) => {
     if (err) {
-      console.log(`Error: ${err}`)
-      console.log(`Bot broken: ${botName}`)
-      return
+      console.log(`Error: ${err}`);
+      console.log(`Bot broken: ${botName}`);
+      return;
     }
 
     if (stdout) {
-      console.log(`Stdout: ${stdout}`)
+      console.log(`Stdout: ${stdout}`);
       if (autoRestart) {
-        startBot(botName, password)
+        startBot(botName, password);
       }
     }
 
     if (stderr) {
-      console.log(`Stderr: ${stderr}`)
+      console.log(`Stderr: ${stderr}`);
     }
-  })
+  });
 }
 
 const botsToStart = [
   // { username: 'Sephi' }
-  { username: 'MinerX+' },
-  { username: 'MinerX-' },
-  { username: 'MinerZ+' },
-  { username: 'MinerZ-' },
-  { username: 'MinerY1' },
-  { username: 'MinerY2' },
-  { username: 'MinerY3' },
-  { username: 'MinerY4' }
+  { username: "MinerX+" },
+  { username: "MinerX-" },
+  { username: "MinerZ+" },
+  { username: "MinerZ-" },
+  { username: "MinerY1" },
+  { username: "MinerY2" },
+  { username: "MinerY3" },
+  { username: "MinerY4" },
   // { username: 'Breeder' },
   // { username: 'Sorter' },
   // { username: 'Guard1' },
@@ -48,48 +49,50 @@ const botsToStart = [
   // { username: 'Archer1' },
   // { username: 'Archer2' },
   // { username: 'Archer3' }
-]
+];
 
-let i = 0
-function runNextBot () {
-  const botToStart = botsToStart[i]
-  i++
+let i = 0;
+function runNextBot() {
+  const botToStart = botsToStart[i];
+  i++;
   if (i <= botsToStart.length) {
     setTimeout(() => {
-      startBot(botToStart.username)
-      runNextBot()
-    }, 3000)
+      startBot(botToStart.username);
+      runNextBot();
+    }, 3000);
   }
-};
+}
 
-runNextBot()
+runNextBot();
 
 // Master websocket for load bots
-const { webServer, webServerPort, webServerPassword } = require('@config')
-const io = require('socket.io-client')
-const socket = io(webServer + ':' + webServerPort)
-let loged = false
+const { webServer, webServerPort, webServerPassword } = require("@config");
+const io = require("socket.io-client");
+const socket = io(webServer + ":" + webServerPort);
+let loged = false;
 
-socket.on('connect', () => {
-  console.log('Connected to webserver')
-  socket.emit('botMaster', 'on')
-  socket.emit('login', webServerPassword)
-})
+socket.on("connect", () => {
+  console.log("Connected to webserver");
+  socket.emit("botMaster", "on");
+  socket.emit("login", webServerPassword);
+});
 
-socket.on('login', (authenticate) => {
+socket.on("login", (authenticate) => {
   if (authenticate.auth) {
-    loged = true
+    loged = true;
   } else {
-    loged = false
+    loged = false;
   }
-})
+});
 
-socket.on('disconnect', () => {
-  console.log('disconnected from webserver')
-})
+socket.on("disconnect", () => {
+  console.log("disconnected from webserver");
+});
 
-socket.on('botConnect', data => {
-  if (!loged) { return }
-  console.log(`Starting bot ${data.botName}`)
-  startBot(data.botName, data.botPassword)
-})
+socket.on("botConnect", (data) => {
+  if (!loged) {
+    return;
+  }
+  console.log(`Starting bot ${data.botName}`);
+  startBot(data.botName, data.botPassword);
+});
