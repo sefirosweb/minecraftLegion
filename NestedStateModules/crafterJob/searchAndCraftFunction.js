f5ef56cb5146e9d2afb001783117955573c834b7
Sefirosweb <sefirosweb@gmail.com>
2022-01-30 09:21:06 +0100

    added function for search and craft items

--- NestedStateModules/crafterJob/searchAndCraftFunction.js
+++ NestedStateModules/crafterJob/searchAndCraftFunction.js
@@ -3,6 +3,7 @@ const {
   BehaviorIdle,
   NestedStateMachine,
 } = require("mineflayer-statemachine");
+const BehaviorCraft = require("@BehaviorModules/BehaviorCraft");
 
 function searchAndCraftFunction(bot, targets) {
   const { getItemsToPickUp } = require("@modules/craftModule")(bot);
@@ -14,13 +15,18 @@ function searchAndCraftFunction(bot, targets) {
 
   const exit = new BehaviorIdle(targets);
   exit.stateName = "exit";
-  exit.x = 125;
-  exit.y = 113;
+  exit.x = 325;
+  exit.y = 313;
 
   const checkPickup = new BehaviorIdle(targets);
   checkPickup.stateName = "checkPickup";
   checkPickup.x = 125;
-  checkPickup.y = 113;
+  checkPickup.y = 413;
+
+  const craftItem = new BehaviorCraft(bot, targets);
+  craftItem.stateName = "Craft Item";
+  craftItem.x = 525;
+  craftItem.y = 413;
 
   const pickUpItems = require("@NestedStateModules/getReady/pickUpItems")(
     bot,
@@ -28,9 +34,9 @@ function searchAndCraftFunction(bot, targets) {
   );
   pickUpItems.stateName = "Pick Up Items";
   pickUpItems.x = 325;
-  pickUpItems.y = 50;
+  pickUpItems.y = 113;
 
-  let checkPickupItems;
+  let recipes = [];
 
   const transitions = [
     new StateTransition({
@@ -38,6 +44,10 @@ function searchAndCraftFunction(bot, targets) {
       child: checkPickup,
       onTransition: () => {
         checkPickupItems = getItemsToPickUp("iron_sword", targets.chests);
+        targets.pickUpItems = checkPickupItems
+          ? checkPickupItems.itemToPickup
+          : false;
+        recipes = checkPickupItems ? checkPickupItems.repicesUsed : [];
       },
       shouldTransition: () => true,
     }),
@@ -45,22 +55,56 @@ function searchAndCraftFunction(bot, targets) {
     new StateTransition({
       parent: checkPickup,
       child: exit,
-      shouldTransition: () => !checkPickupItems,
+      shouldTransition: () => {
+        !targets.pickUpItems && targets.pickUpItems.length === 0;
+      },
+    }),
+
+    new StateTransition({
+      parent: checkPickup,
+      child: craftItem,
+      onTransition: () => {
+        targets.craftItem = {
+          name: recipes.shift().result.name,
+        };
+      },
+      shouldTransition: () =>
+        targets.pickUpItems && targets.pickUpItems.length === 0,
     }),
 
     new StateTransition({
       parent: checkPickup,
       child: pickUpItems,
-      onTransition: () => {
-        targets.pickUpItems = checkPickupItems.itemToPickup;
-      },
-      shouldTransition: () => checkPickupItems,
+      shouldTransition: () =>
+        targets.pickUpItems && targets.pickUpItems.length > 0,
     }),
 
     new StateTransition({
       parent: pickUpItems,
-      child: exit,
+      child: craftItem,
+      onTransition: () => {
+        targets.craftItem = {
+          name: recipes.shift().result.name,
+        };
+      },
       shouldTransition: () => pickUpItems.isFinished(),
+    }),
+
+    new StateTransition({
+      parent: craftItem,
+      child: craftItem,
+      onTransition: () => {
+        targets.craftItem = {
+          name: recipes.shift().result.name,
+        };
+      },
+      shouldTransition: () => recipes.length > 0 && craftItem.isFinished(),
+    }),
+
+    new StateTransition({
+      parent: craftItem,
+      child: exit,
+      shouldTransition: () => recipes.length === 0 && craftItem.isFinished(),
     }),
   ];
 
