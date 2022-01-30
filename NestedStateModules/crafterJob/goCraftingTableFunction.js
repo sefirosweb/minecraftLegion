c4041044b2514c75369fa0eb508211578c117217
Sefirosweb <sefirosweb@gmail.com>
2022-01-30 11:36:30 +0100

    fixed layout statemachine

--- NestedStateModules/crafterJob/goCraftingTableFunction.js
+++ NestedStateModules/crafterJob/goCraftingTableFunction.js
@@ -7,19 +7,27 @@ const BehaviorMoveTo = require("@BehaviorModules/BehaviorMoveTo");
 
 function goCraftingTableFunction(bot, targets) {
   const mcData = require("minecraft-data")(bot.version);
-  
+
   const start = new BehaviorIdle(targets);
   start.stateName = "Start";
+  start.x = 125;
+  start.y = 113;
 
   const exit = new BehaviorIdle(targets);
   exit.stateName = "exit";
+  exit.x = 125;
+  exit.y = 213;
 
   const checkCraftingTable = new BehaviorIdle(targets);
-  checkCraftingTable.stateName = "checkCraftingTable";
+  checkCraftingTable.stateName = "Check near crafting table";
+  checkCraftingTable.x = 325
+  checkCraftingTable.y = 113
 
   const goTable = new BehaviorMoveTo(bot, targets);
   goTable.stateName = "Go crafting table";
   goTable.movements = targets.movements;
+  goTable.x = 325
+  goTable.y = 213
 
   let craftingTable = false;
 
