3c69b370d444d6248e87f9c9229b0faef7ace963
Sefirosweb <sefirosweb@gmail.com>
2022-01-30 02:50:42 +0100

    fixed bug in inventory items

--- modules/craftModule.js
+++ modules/craftModule.js
@@ -144,7 +144,7 @@ module.exports = function (bot) {
         item = recipe.items[i];
 
         const invItem = currentInventoryStatus.find(
-          (inv) => (inv.id = item.id)
+          (inv) => (inv.type === item.type)
         );
 
         if (invItem) {
