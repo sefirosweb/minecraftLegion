e99e073cf54199baf8288d7a37601cb11eeb09b2
Sefirosweb <sefirosweb@gmail.com>
2022-03-12 00:22:41 +0100

    Refactorize behavior for eat fixed bugs

--- BehaviorModules/BehaviorEatFood.js
+++ BehaviorModules/BehaviorEatFood.js
@@ -1,121 +1,94 @@
-const botWebsocket = require('@modules/botWebsocket')
+const botWebsocket = require("@modules/botWebsocket");
 
 module.exports = class BehaviorEatFood {
-  constructor (bot, targets, foods = []) {
-    this.bot = bot
-    this.targets = targets
-    this.stateName = 'BehaviorEatFood'
-    this.isEndEating = false
-
-    this.isEating = false
+  constructor(bot, targets, foods = []) {
+    this.bot = bot;
+    this.targets = targets;
+    this.stateName = "BehaviorEatFood";
+    this.finished = false;
   }
 
-  onStateEntered () {
+  onStateEntered() {
     if (this.bot.food === 20) {
-      this.isEndEating = true
+      this.finished = true;
     } else {
-      this.eat()
+      this.eat();
     }
   }
 
-  getAllFoods () {
-    const mcData = require('minecraft-data')(this.bot.version)
-    const mcDataFoods = mcData.foodsArray
-    return mcDataFoods.map((item) => item.name)
+  getAllFoods() {
+    const mcData = require("minecraft-data")(this.bot.version);
+    const mcDataFoods = mcData.foodsArray;
+    return mcDataFoods.map((item) => item.name);
   }
 
-  onStateExited () {
-    this.isEating = false
-    this.isEndEating = false
+  onStateExited() {
+    this.finished = false;
   }
 
-  checkFoodInInventory () {
+  checkFoodInInventory() {
     return this.bot.inventory.items().reduce((validFood, foodInventory) => {
-      const returnValidFood = [...validFood]
+      const returnValidFood = [...validFood];
 
-      const priority = this.targets.config.itemsCanBeEat.findIndex(food => food === foodInventory.name)
+      const priority = this.targets.config.itemsCanBeEat.findIndex(
+        (food) => food === foodInventory.name
+      );
 
       if (priority >= 0) {
         const validFoodWithPriority = {
           ...foodInventory,
-          priority
-        }
-        returnValidFood.push(validFoodWithPriority)
+          priority,
+        };
+        returnValidFood.push(validFoodWithPriority);
       }
 
-      return returnValidFood
-    }, [])
+      return returnValidFood;
+    }, []);
   }
 
-  eat () {
-    this.isEating = true
-
+  eat() {
     const availableFood = this.checkFoodInInventory().sort(function (a, b) {
       if (a.priority > b.priority) {
-        return 1
+        return 1;
       }
       if (a.priority < b.priority) {
-        return -1
+        return -1;
       }
-      return 0
-    })
+      return 0;
+    });
 
     if (availableFood.length === 0) {
-      botWebsocket.log('No food in inventory ')
-      this.isEndEating = true
-      return
+      botWebsocket.log("No food in inventory ");
+      this.finished = true;
+      return;
     }
 
-    const firstFoodFound = availableFood.shift()
+    const firstFoodFound = availableFood.shift();
 
     if (!firstFoodFound) {
-      this.isEndEating = true
-      return
+      this.finished = true;
+      return;
     }
 
-    this.equipFood(firstFoodFound)
-      .then(() => this.consumeFood())
+    this.bot
+      .equip(firstFoodFound, "hand")
       .then(() => {
-        this.isEating = false
-        if (!this.bot.food === 20) {
-          this.eat()
+        return this.bot.consume();
+      })
+      .then(() => {
+        if (!this.bot.food !== 20) {
+          this.eat();
         } else {
-          this.isEndEating = true
+          this.finished = true;
         }
       })
       .catch((error) => {
-        botWebsocket.log('Error on eat food ' + error)
-        setTimeout(() => {
-          this.isEndEating = true
-        }, 500)
-      })
+        botWebsocket.log("Error on eat food " + error);
+        this.finished = true;
+      });
   }
 
-  consumeFood () {
-    return new Promise((resolve, reject) => {
-      this.bot.consume((error) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
-      })
-    })
+  isFinished() {
+    return this.finished;
   }
-
-  equipFood (food) {
-    return new Promise((resolve, reject) => {
-      this.bot.equip(food, 'hand', function (error) {
-        if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
-      })
-    })
-  }
-
-  isFinished () {
-    return this.isEndEating
-  }
-}
+};
