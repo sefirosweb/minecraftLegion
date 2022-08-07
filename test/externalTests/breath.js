48ad6bd22aa298ceadbad60660e684a47da21ebc
Sefirosweb <sefirosweb@gmail.com>
2022-08-07 09:41:08 +0200

    Deleted mineflayer tests

--- test/externalTests/breath.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-module.exports = () => async (bot) => {
-  await bot.waitForChunksToLoad()
-  // TODO: ** Fix in 1.17
-  if (bot.oxygenLevel) assert.strictEqual(bot.oxygenLevel, 20, 'Wrong oxygen level')
-}
