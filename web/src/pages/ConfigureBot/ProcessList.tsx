043aea551188acb9d25a585fa74a4dfa70824151
Sefirosweb <sefirosweb@gmail.com>
2024-03-31 11:37:12 +0200

    change processlist

--- web/src/pages/ConfigureBot/ProcessList.tsx
+++ web/src/pages/ConfigureBot/ProcessList.tsx
@@ -1,22 +1,31 @@
-import { BotSelectedContext } from "./ConfigurationContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useStore } from "@/hooks/useStore";
 
 export const ProcessList: React.FC = () => {
-  const botConfig = useContext(BotSelectedContext);
+  const [socket] = useStore(state => [state.socket])
+  const [events, setEvents] = useState<string[]>([])
+
+  useEffect(() => {
+    // pending to fix
+    socket?.on('receiveDirectMessage', ({ type, message }: { type: string, message: Array<string> }) => {
+      if (type !== 'current_events') return
+      setEvents(message)
+    });
+
+    return () => {
+      socket?.off("receiveDirectMessage");
+    }
+  }, [setEvents])
 
   return (
     <>
-      <div className='row'>
-        <div className='col-12'>
-          <h4>List of all events used by bot</h4>
-          <div className='border border-warning'>
-            <ul>
-              {botConfig.events.map((e, i) => (
-                <li key={i}>{e}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
+      <h4>List of all events used by bot</h4>
+      <div className='border border-warning'>
+        <ul>
+          {events.map((e, i) => (
+            <li key={i}>{e}</li>
+          ))}
+        </ul>
       </div>
     </>
   )
