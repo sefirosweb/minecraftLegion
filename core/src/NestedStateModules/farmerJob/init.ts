8fba01b0f612f7c3ca6509e9ee408e62eb28801d
Sefirosweb <sefirosweb@gmail.com>
2024-04-01 23:01:41 +0200

    Optimized breeder

--- core/src/NestedStateModules/farmerJob/init.ts
+++ core/src/NestedStateModules/farmerJob/init.ts
@@ -1,20 +1,15 @@
 import { StateTransition, BehaviorIdle, NestedStateMachine } from 'mineflayer-statemachine'
-import BehaviorGetReady from '@/BehaviorModules/BehaviorGetReady'
 import BehaviorEatFood from '@/BehaviorModules/BehaviorEatFood'
 import { LegionStateMachineTargets } from 'base-types'
 import getClosestEnemy from '@/modules/getClosestEnemy'
 import GoChestsFunctions from '@/NestedStateModules/getReady/goChestsFunctions'
 import FarmingFunction from '@/NestedStateModules/farmerJob/farmingFunction'
+import GetReadyFunction from '@/NestedStateModules/getReady/getReadyFunction'
 import CombatStrategyFunction from '@/NestedStateModules/combat/combatStrategyFunction'
 import { Bot } from 'mineflayer'
 
 export default (bot: Bot, targets: LegionStateMachineTargets) => {
-  const start = new BehaviorIdle()
-  start.stateName = 'Start'
-  start.x = 125
-  start.y = 113
-
-  const getReady = new BehaviorGetReady(bot, targets)
+  const getReady = GetReadyFunction(bot, targets)
   getReady.stateName = 'Get Ready'
   getReady.x = 125
   getReady.y = 213
@@ -40,31 +35,9 @@ export default (bot: Bot, targets: LegionStateMachineTargets) => {
 
   const transitions = [
     new StateTransition({
-      parent: start,
-      child: getReady,
-      onTransition: () => {
-        targets.entity = undefined
-      },
-      shouldTransition: () => true
-    }),
-
-    new StateTransition({
-      parent: getReady,
-      child: goChests,
-      shouldTransition: () => !getReady.getIsReady() || bot.inventory.items().length >= 33
-    }),
-
-    new StateTransition({
-      parent: goChests,
-      child: getReady,
-      name: 'Go to chests',
-      shouldTransition: () => goChests.isFinished()
-    }),
-
-    new StateTransition({
       parent: getReady,
       child: eatFood,
-      shouldTransition: () => getReady.getIsReady() && bot.inventory.items().length < 33
+      shouldTransition: () => getReady.isFinished()
     }),
 
     new StateTransition({
@@ -77,6 +50,12 @@ export default (bot: Bot, targets: LegionStateMachineTargets) => {
       parent: farming,
       child: goChests,
       shouldTransition: () => farming.isFinished()
+    }),
+
+    new StateTransition({
+      parent: goChests,
+      child: getReady,
+      shouldTransition: () => goChests.isFinished()
     }),
 
     new StateTransition({
@@ -97,7 +76,7 @@ export default (bot: Bot, targets: LegionStateMachineTargets) => {
 
   ]
 
-  const nestedState = new NestedStateMachine(transitions, start)
+  const nestedState = new NestedStateMachine(transitions, getReady)
   nestedState.stateName = 'Farmer Job'
   return nestedState
 }
